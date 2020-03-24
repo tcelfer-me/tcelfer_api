b95cf9d00534cb60c1e7dd73cb6e5ac83a91d628
Anthony Gargiulo <anthony@agargiulo.com>
2020-03-24 01:25:42 -0700

    fixes for some scripting

--- .ci/test_user_gen.zsh
+++ .ci/test_user_gen.zsh
@@ -8,14 +8,14 @@ emails=( user1@example.com user2@example.com user3@example.com )
 for email in $emails; do
   for user in $usernames; do
     ${=httpie} POST http://localhost:9292/api/v1/user/new username="$user" email="$email" password='hunter2hunter2'
-done
+  done
 done
 
 echo 'This creates 2 users without email addresses'
 echo 'They should both work, and not return 409'
 usernames=( no_email_1 no_email_2 )
 for user in $usernames; do
-  http -j --follow POST http://localhost:9292/api/v1/user/new username="$user" password='hunter2hunter2'
+  ${=httpie} POST http://localhost:9292/api/v1/user/new username="$user" password='hunter2hunter2'
 done
 
 echo 'pp TcelferApi::User.map { {user: _1.username, email: _1.email} };' | bundle exec bin/console
